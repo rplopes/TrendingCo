2b26b164f2792ecf5053ce6b986587a7826a2033
Ricardo Lopes <mail@ricardolopes.net>
2012-04-18 10:43:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trendingco::Application.config.secret_token = '94eb127b11b8fc3e4a197cdd1519714b46cd3f07effbe8115fc325d5afc8ecb771ef713de910cbba3d7afab2aa34cb691ea27a0f7004d3f75133eeacd5d1e847'
